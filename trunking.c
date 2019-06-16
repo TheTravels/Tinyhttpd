03c342ea4eb43fc132605b76967ff27ae6f8297b
merafour <merafour@163.com>
2019-06-17 03:51:31 +0800

    SocketThreadViewer::doWork

--- trunking.c
+++ trunking.c
@@ -2,7 +2,12 @@
 #include "thread_list.h"
 #include "agreement/storage_pack.h"
 #include <unistd.h>
-//#include <sys/socket.h>
+#include <string.h>
+
+#define LINUX_BUILD   1
+#if LINUX_BUILD
+#include <sys/socket.h>
+#endif
 
 static int trunk_flag = 0;
 void trunking_exit(void)
@@ -13,9 +18,14 @@ void trunking_exit(void)
 static char buffer[4096];
 static void trunking_call(const int client, const struct device_list* const device, const uint8_t pack[], const uint16_t _psize)
 {
+#if LINUX_BUILD
     json_device(buffer, sizeof(buffer), device, pack, _psize);
-//    send(client, buffer, strlen(buffer), 0);
-    printf("send:%s\n", buffer);
+    send(client, buffer, strlen(buffer), 0);
+    //printf("send:%s\n", buffer);
+#else
+    (void)client;
+    json_device(buffer, sizeof(buffer), device, pack, _psize);
+#endif
 }
 
 void trunking(const int client)
