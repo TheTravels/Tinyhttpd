c28ba8724bed7efb3b205e270e12a4864422005c
merafour <merafour@163.com>
2019-09-16 16:44:02 +0800

    pull #define numchars2

---
+++ pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#echo $#
+#echo $@
+
+#if (("$#" < 1)) ; then
+#echo "Please input commit info"
+#exit
+#else
+#echo "commit is " $1
+#make clean
+#git add .
+#git commit -m "$1"
+#git push origin OBD
+#fi
+
+cd OBD_Report
+git fetch --all
+git reset --hard origin/master
+
+
